0c33625def0a98fe0d0c9813053d31d5f4d54caf
suryanalam <suryanalam3011@gmail.com>
2025-09-02 00:37:52 +0530

    update backend baseUrl

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://notes-app-0wxo.onrender.com",
+  baseURL: "https://notes-app-0wxo.onrender.com/api",
   timeout: 5000,
   withCredentials: true,
   headers: {
